fdde3f61ba74bd4237b13412a74143dfe36d78dc
Nathan Westfall <nathan@fistbumpstudios.com>
2018-08-18 21:28:38 -0400

    started adding pop up for settings

---
+++ Clad/SettingsPopupView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+
+namespace Clad
+{
+    [Register(nameof(SettingsPopupView))]
+    public class SettingsPopupView : UIView
+    {
+        /*
+         * Planes about to land come back later and finish this
+         * 
+         * Some settings ideas...
+         * Pad fade time
+         * Pad crossfade time
+         * Pad - L/R
+         * Click - L/R
+         * 
+         * */
+    }
+}
